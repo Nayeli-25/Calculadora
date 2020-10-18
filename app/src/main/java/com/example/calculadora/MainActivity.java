69ecff2aaa3e128477d5fb35d4e256df0a0dbcb4
Nayeli <nayeliesquivelluna@gmail.com>
2020-10-18 14:48:53 -0500

    edité las funciones

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,242 +1,151 @@
 package com.example.calculadora;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvOperacion;
-    double resultado=0;
-    boolean cambiarSigno=false;
-    boolean porcentaje=false;
-    boolean suma=false;
-    boolean resta=false;
-    boolean multiplicacion=false;
-    boolean division=false;
-    String numeros="";
+    double n1, n2, resultado;
+    String operador;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPunto, btnSuma, btnResta, btnMult, btnDiv, btnIgual, btnLimpiar, btnPorcentaje, btnCambiar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.tvOperacion=findViewById(R.id.tvOperacion);
+
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btnPunto = (Button) findViewById(R.id.btnPunto);
+        btnSuma = (Button) findViewById(R.id.btnSuma);
+        btnResta = (Button) findViewById(R.id.btnResta);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnIgual = (Button) findViewById(R.id.btnIgual);
+        btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
+        btnPorcentaje = (Button) findViewById(R.id.btnPorcentaje);
+        btnCambiar = (Button) findViewById(R.id.btnCambiar);
+        tvOperacion = (TextView) findViewById(R.id.tvOperacion);
     }
-    public void concatenar(View view) {
-        switch (view.getId())
-        {
+    public void Concatenar(View view){
+        String tvConcatenar=tvOperacion.getText().toString();
+        switch (view.getId()){
             case R.id.btn0:
-                if (numeros!="")
-                {
-                    numeros = numeros+"0";
-                    tvOperacion.setText(numeros);
-                }
-                else {tvOperacion.setText("0");}
+                tvOperacion.setText(tvConcatenar+"0");
                 break;
             case R.id.btn1:
-                numeros = numeros+"1";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"1");
                 break;
             case R.id.btn2:
-                numeros = numeros+"2";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"2");
                 break;
             case R.id.btn3:
-                numeros = numeros+"3";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"3");
                 break;
             case R.id.btn4:
-                numeros = numeros+"4";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"4");
                 break;
             case R.id.btn5:
-                numeros = numeros+"5";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"5");
                 break;
             case R.id.btn6:
-                numeros = numeros+"6";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"6");
                 break;
             case R.id.btn7:
-                numeros = numeros+"7";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"7");
                 break;
             case R.id.btn8:
-                numeros = numeros+"8";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"8");
                 break;
             case R.id.btn9:
-                numeros = numeros+"9";
-                tvOperacion.setText(numeros);
+                tvOperacion.setText(tvConcatenar+"9");
                 break;
             case R.id.btnPunto:
-                if (numeros=="")
-                {
-                    numeros = numeros+"0.";
-                    tvOperacion.setText(numeros);
-                }
-                else
-                {
-                    numeros = numeros+".";
-                    tvOperacion.setText(numeros);
-                }
-                break;
-            default:
+                tvOperacion.setText(tvConcatenar+".");
                 break;
         }
-    }
-    public void cambiarSigno(View view){
-        if(numeros!="") {
-            if(resultado==0){
-                this.resultado= Double.parseDouble(numeros);
-            }else {
-                this.resultado = resultado * (Double.parseDouble(numeros)*-1);
-            }
-        }
-        cambiarSigno=false;
-        porcentaje=true;
-        suma = false;
-        resta = false;
-        multiplicacion = false;
-        division = false;
-        tvOperacion.setText(numeros);
-        numeros = "";
-    }
-    public void porcentaje(View view){
-        if(numeros!="") {
-            if(resultado==0){
-                this.resultado= Double.parseDouble(numeros);
-            }else {
-                this.resultado = resultado * (Double.parseDouble(numeros)*0.01);
-            }
-        }
-        porcentaje=true;
-        suma = false;
-        resta = false;
-        multiplicacion = false;
-        division = false;
-        tvOperacion.setText(numeros);
-        numeros = "";
-    }
-    public void sumar(View view)
-    {
-        if(numeros!="") {
-            this.resultado =resultado+Double.parseDouble(numeros);
-        }
-        porcentaje=false;
-        suma = true;
-        resta = false;
-        multiplicacion = false;
-        division = false;
-        tvOperacion.setText(numeros);
-        numeros = "";
-    }
-    public void restar(View view) {
-        if(numeros!="") {
-            if(resultado==0){
-                this.resultado= Double.parseDouble(numeros);
-            }else {
-                this.resultado = resultado - Double.parseDouble(numeros);
-            }
-        }
-        porcentaje=false;
-        suma = false;
-        resta = true;
-        multiplicacion = false;
-        division = false;
-        tvOperacion.setText(numeros);
-        numeros = "";
 
     }
-    public void multiplicar(View view) {
-        if(resultado==0){
-            this.resultado= Double.parseDouble(numeros);
-        }else {
-            this.resultado = resultado * Double.parseDouble(numeros);
-        }
-        porcentaje=false;
-        suma = false;
-        resta = false;
-        multiplicacion = true;
-        division = false;
-        tvOperacion.setText(numeros);
-        numeros = "";
-    }
-    public void dividir(View view) {
-        if(resultado==0){
-            this.resultado= Double.parseDouble(numeros);
-        }else {
-            try {
-                this.resultado = resultado / Double.parseDouble(numeros);
-            }catch (Exception ex){
-                Toast.makeText(this,ex.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-
-        }
-        porcentaje=false;
-        suma = false;
-        resta = false;
-        multiplicacion = false;
-        division = true;
-        tvOperacion.setText(numeros);
-        numeros = "";
-
-    }
-
-    public void igual(View view){
-        if (cambiarSigno=true) {
-            this.resultado = resultado * (Double.parseDouble(numeros)*-1);
-            tvOperacion.setText(String.valueOf(resultado));
-        }
-        if (porcentaje=true) {
-            this.resultado = resultado * (Double.parseDouble(numeros)*0.01);
-            tvOperacion.setText(String.valueOf(resultado));
-        }
-        if (suma==true)
-        {
-            this.resultado =resultado+Double.parseDouble(numeros);
-            tvOperacion.setText(String.valueOf(resultado));
-        }
-        else if (resta==true)
-        {
-            this.resultado =resultado-Double.parseDouble(numeros);
-            tvOperacion.setText(String.valueOf(resultado));
-        }
-        else if (multiplicacion==true)
-        {
-            this.resultado =resultado*Double.parseDouble(numeros);
-            tvOperacion.setText(String.valueOf(resultado));
-        }
-        else
-        {
-
-            if (division==true)
-            {
-                try {
-                    this.resultado = resultado / Double.parseDouble(numeros);
-                    tvOperacion.setText(String.valueOf(resultado));
-                }catch (Exception e){
-                    Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
-                    tvOperacion.setText("Error");
-                }
-            }
-        }
-
-    }
-
-    public void limpiar(View view)
-    {
-        numeros="";
+    public  void Limpiar(View view) {
+        n1=0;
+        n2=0;
+        tvOperacion.setText("");
         resultado=0;
-        suma=false;
-        resta=false;
-        multiplicacion=false;
-        division=false;
-        tvOperacion.setText("0");
+    }
+    public  void CambiarSigno(View view){
+        n1=Double.parseDouble(tvOperacion.getText().toString());
+        operador="+/-";
+        n1=n1*-1;
+        tvOperacion.setText(String.valueOf(n1));
+    }
+    public  void Porcentaje(View view){
+        n1=Double.parseDouble(tvOperacion.getText().toString());
+        operador="%";
+        tvOperacion.setText("");
+    }
+    public  void Dividir(View view){
+        n1=Double.parseDouble(tvOperacion.getText().toString());
+        operador="÷";
+        tvOperacion.setText("");
+    }
+    public  void Multiplicar(View view){
+        n1=Double.parseDouble(tvOperacion.getText().toString());
+        operador="x";
+        tvOperacion.setText("");
+    }
+    public  void Restar(View view){
+        n1=Double.parseDouble(tvOperacion.getText().toString());
+        operador="-";
+        tvOperacion.setText("");
+    }
+    public  void Sumar(View view){
+        n1=Double.parseDouble(tvOperacion.getText().toString());
+        operador="+";
+        tvOperacion.setText("");
     }
 
+    public  void Resultado(View view) {
 
+        switch (operador) {
+            case "+":
+                n2 = Double.parseDouble(tvOperacion.getText().toString());
+                resultado=n1+n2;
+                tvOperacion.setText(String.valueOf(resultado));
+                break;
+            case "-":
+                n2 = Double.parseDouble(tvOperacion.getText().toString());
+                resultado=n1-n2;
+                tvOperacion.setText(String.valueOf(resultado));
+                break;
+            case "x":
+                n2 = Double.parseDouble(tvOperacion.getText().toString());
+                resultado=n1*n2;
+                tvOperacion.setText(String.valueOf(resultado));
+                break;
+            case "÷":
+                n2 = Double.parseDouble(tvOperacion.getText().toString());
+                resultado=n1/n2;
+                tvOperacion.setText(String.valueOf(resultado));
+                break;
+            case "%":
+                n2 = Double.parseDouble(tvOperacion.getText().toString());
+                resultado=(n2*n1)/100;
+                tvOperacion.setText(String.valueOf(resultado));
+                break;
+
+        }
+
+    }
 }
